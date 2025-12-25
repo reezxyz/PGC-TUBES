6e852a32e96581ae4f0b75307017810d52cbce02
reez 45 <rizkyabdurrahman455@gmail.com>
2025-12-25 14:46:30 +0700

    Player : Add movement and crouch

--- Player/PlayerLook.cs
+++ Player/PlayerLook.cs
@@ -3,17 +3,33 @@ using UnityEngine;
 public class PlayerLook : MonoBehaviour
 {
     public float mouseSensitivity = 150f;
-
     public Transform playerBody;
+
+    public float standCameraHeight = 1.6f;
+    public float crouchCameraHeight = 1.0f;
+    public float cameraSmooth = 8f;
+
     float xRotation = 0f;
+    float currentCameraHeight;
+
+    PlayerMovement movement;
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+        movement = playerBody.GetComponent<PlayerMovement>();
+        currentCameraHeight = standCameraHeight;
     }
 
     void Update()
+    {
+        HandleMouseLook();
+        HandleCameraHeight();
+    }
+
+    void HandleMouseLook()
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
@@ -23,5 +39,16 @@ public class PlayerLook : MonoBehaviour
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    void HandleCameraHeight()
+    {
+        float targetHeight = movement != null && movement.enabled && 
+                             Input.GetKey(KeyCode.C) 
+                             ? crouchCameraHeight 
+                             : standCameraHeight;
+
+        currentCameraHeight = Mathf.Lerp(currentCameraHeight, targetHeight, Time.deltaTime * cameraSmooth);
+        transform.localPosition = new Vector3(0, currentCameraHeight, 0);
     }
 }
--- Player/PlayerMovement.cs
+++ Player/PlayerMovement.cs
@@ -1,18 +1,71 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float walkSpeed = 4f;
+    public float crouchSpeed = 2f;
+    public float gravity = -9.8f;
+
+    public float standHeight = 1.8f;
+    public float crouchHeight = 1.0f;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private bool isCrouching;
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+        controller.height = standHeight;
+        controller.center = new Vector3(0, standHeight / 2f, 0);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        HandleCrouch();
+        HandleMovement();
+        HandleGravity();
+    }
+
+    void HandleMovement()
+    {
+        float speed = isCrouching ? crouchSpeed : walkSpeed;
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+    }
+
+    void HandleCrouch()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            isCrouching = !isCrouching;
+
+            if (isCrouching)
+            {
+                controller.height = crouchHeight;
+                controller.center = new Vector3(0, crouchHeight / 2f, 0);
+            }
+            else
+            {
+                controller.height = standHeight;
+                controller.center = new Vector3(0, standHeight / 2f, 0);
+            }
+        }
+    }
+
+    void HandleGravity()
+    {
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
